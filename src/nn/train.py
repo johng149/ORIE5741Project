76a6743161f889cc501ff439e37af1a948f7088d
jg843 <jg843@cornell.edu>
2024-05-03 18:11:03 -0400

    Save on interrupt

--- src/nn/train.py
+++ src/nn/train.py
@@ -76,57 +76,71 @@ def train(
         writer = SummaryWriter(writer)
     train_iter = iter(train_dl)
     test_iter = iter(test_dl) if test_dl else None
-    for epoch in tqdm(range(seen_epochs, target_epochs)):
-        model.train()
-        try:
-            emb, pos_indices, targets, masks = next(train_iter)
-        except StopIteration:
-            train_iter = iter(train_dl)
-            emb, pos_indices, targets, masks = next(train_iter)
-        outputs = model(emb, pos_indices, mask=masks)
-        loss = F.cross_entropy(
-            outputs.reshape(-1, outputs.size(-1)),
-            targets.reshape(-1),
-            ignore_index=ignore_index,
-        )
-        optm.zero_grad()
-        loss.backward()
-        optm.step()
-        writer.add_scalar("Loss/train", loss.item(), epoch)
-        writer.add_scalar(
-            "Accuracy/train", accuracy(outputs.argmax(-1), targets), epoch
-        )
-        if epoch % test_every_n == 0 and test_data:
-            with torch.no_grad():
-                model.eval()
-                try:
-                    emb, pos_indices, targets, masks = next(test_iter)
-                except StopIteration:
-                    test_iter = iter(test_dl)
-                    emb, pos_indices, targets, masks = next(test_iter)
-                outputs = model(emb, pos_indices, mask=masks)
-                loss = F.cross_entropy(
-                    outputs.reshape(-1, outputs.size(-1)),
-                    targets.reshape(-1),
-                    ignore_index=ignore_index,
-                )
-                writer.add_scalar("Loss/test", loss.item(), epoch)
-                writer.add_scalar(
-                    "Accuracy/test", accuracy(outputs.argmax(-1), targets), epoch
-                )
-        if epoch % save_every_n == 0:
-            save_data = {
-                "model": model.state_dict(),
-                "optimizer": optm.state_dict(),
-                "epoch": epoch,
-                "model_kwargs": model.kwargs,
-                "train_kwargs": train_data.kwargs,
-                "test_kwargs": test_data.kwargs if test_data else None,
-                "writer_dir": writer.get_logdir(),
-                "checkpoint_path": checkpoint_path,
-                "ignore_index": ignore_index,
-            }
-            torch.save(save_data, checkpoint_path)
+    try:
+        for epoch in tqdm(range(seen_epochs, target_epochs)):
+            model.train()
+            try:
+                emb, pos_indices, targets, masks = next(train_iter)
+            except StopIteration:
+                train_iter = iter(train_dl)
+                emb, pos_indices, targets, masks = next(train_iter)
+            outputs = model(emb, pos_indices, mask=masks)
+            loss = F.cross_entropy(
+                outputs.reshape(-1, outputs.size(-1)),
+                targets.reshape(-1),
+                ignore_index=ignore_index,
+            )
+            optm.zero_grad()
+            loss.backward()
+            optm.step()
+            writer.add_scalar("Loss/train", loss.item(), epoch)
+            writer.add_scalar(
+                "Accuracy/train", accuracy(outputs.argmax(-1), targets), epoch
+            )
+            if epoch % test_every_n == 0 and test_data:
+                with torch.no_grad():
+                    model.eval()
+                    try:
+                        emb, pos_indices, targets, masks = next(test_iter)
+                    except StopIteration:
+                        test_iter = iter(test_dl)
+                        emb, pos_indices, targets, masks = next(test_iter)
+                    outputs = model(emb, pos_indices, mask=masks)
+                    loss = F.cross_entropy(
+                        outputs.reshape(-1, outputs.size(-1)),
+                        targets.reshape(-1),
+                        ignore_index=ignore_index,
+                    )
+                    writer.add_scalar("Loss/test", loss.item(), epoch)
+                    writer.add_scalar(
+                        "Accuracy/test", accuracy(outputs.argmax(-1), targets), epoch
+                    )
+            if epoch % save_every_n == 0:
+                save_data = {
+                    "model": model.state_dict(),
+                    "optimizer": optm.state_dict(),
+                    "epoch": epoch,
+                    "model_kwargs": model.kwargs,
+                    "train_kwargs": train_data.kwargs,
+                    "test_kwargs": test_data.kwargs if test_data else None,
+                    "writer_dir": writer.get_logdir(),
+                    "checkpoint_path": checkpoint_path,
+                    "ignore_index": ignore_index,
+                }
+                torch.save(save_data, checkpoint_path)
+    except KeyboardInterrupt:
+        save_data = {
+            "model": model.state_dict(),
+            "optimizer": optm.state_dict(),
+            "epoch": epoch,
+            "model_kwargs": model.kwargs,
+            "train_kwargs": train_data.kwargs,
+            "test_kwargs": test_data.kwargs if test_data else None,
+            "writer_dir": writer.get_logdir(),
+            "checkpoint_path": checkpoint_path,
+            "ignore_index": ignore_index,
+        }
+        torch.save(save_data, checkpoint_path)
 
 
 def load_checkpoint(
